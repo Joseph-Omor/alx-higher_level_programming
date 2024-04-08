4441972ec474391d7e1cdfe1dedecf5860ee7e29
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-04-08 18:33:13 +0100

    Solved for Task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""A program that prints ASCII alphabet, in lowercase"""
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
