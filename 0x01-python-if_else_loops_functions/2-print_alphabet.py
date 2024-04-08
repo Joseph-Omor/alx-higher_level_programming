497f55ec663d369ea7540505984bd73fea65af7f
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-04-08 18:21:16 +0100

    Solved for Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,5 +3,5 @@
 This code prints the ASCII alphabet in lowercase
 not followed by a new line.
 """
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end="")
+for item in 'abcdefghijklmnopqrstuvwxyz':
+    print("{}".format(item), end="")
