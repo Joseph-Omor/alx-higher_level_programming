bc3a6ec282fdb014866d6637caa370e45cabb164
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-04-09 16:41:44 +0100

    deleted incorrectly named file

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-"""A program that prints ASCII alphabet, in lowercase"""
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    if (i != "q" and i != "e"):
-        print("{}".format(i), end="")
