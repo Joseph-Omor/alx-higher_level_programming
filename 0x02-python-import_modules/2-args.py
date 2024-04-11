5a67ab23a6206d8424f2626e1e50d71fda242968
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-04-11 15:46:29 +0100

    added solution for 0x02-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A program that prints the number of and the list of its arguments"""
+if __name__ == "__main__":
+    import sys
+    total = len(sys.argv)
+    if total <= 1:
+        print("0 argument.")
+    else:
+        if total == 2:
+            print("{:d} argument:".format(total -1))
+        else:
+            print("{:d} arguments:".format(total -1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))
