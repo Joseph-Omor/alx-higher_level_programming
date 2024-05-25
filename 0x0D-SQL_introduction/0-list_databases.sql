3a8ac5b8fade0c8a915518a36f7259c168cc354d
Joseph <josephomoruwou@gmail.com>
2024-05-25 03:34:28 -0700

    modified doc for previous submits and solved for 0x0D-2

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,3 @@
 -- A script to list all databases of your MySQL server
+-- SQL Query to list databases!
 SHOW DATABASES;
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- A script to create the databese hbtn_0c_0
+-- SQL Query to create a database!
 CREATE DATABASE hbtn_0c_0;
